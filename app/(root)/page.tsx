c756abb7deee530af3234f6fa77b76c6a1bd70e1
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2024-12-20 11:37:34 +0000

    update home sections

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,18 @@
+import Events from "@/components/pages/Events";
+import Home from "@/components/pages/Home";
 import Footer from "@/components/sections/Footer";
 import Header from "@/components/sections/Header";
 
 export default function RootLayout({
-    children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
       <div className="flex h-screen flex-col">
         <Header/>
-        <main className="flex-1">
-          {children}
+        <main>
+          <Home/>
+          <Events/>
         </main>
         <Footer/>
       </div>
--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <footer className="border-t">
+      <div className="wrapper md:flex-between flex-center flex-col md:flex-row gap-5 text-center">
+        <Link href="/">
+          <Image src="/assets/images/logo.svg" alt="logo" width={120} height={40} />
+        </Link>
+        <p>2024 Events .All rights reserved</p>
+      </div>
+    </footer>
   )
 }
 
