4bafae61d4347ad7078358cd06e44ad4bf599b3e
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-01-07 14:54:52 +0000

    update deploy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,18 +4,21 @@ import Home from "@/components/pages/Home";
 import Footer from "@/components/sections/Footer";
 import Header from "@/components/sections/Header";
 
+// Correct the prop typing for children
 export default function RootLayout({
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div className="flex h-screen flex-col">
-        <Header/>
-        <main>
-          <Home/>
-          <Events/>
-        </main>
-        <Footer/>
-      </div>
-    );
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main>
+        <Home />
+        <Events />
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
