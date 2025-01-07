59a5232f8e6636eb159bb7c5621b5c769a0bf41c
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-01-07 18:58:15 +0000

    fix deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Events from "@/components/pages/Events";
 import Home from "@/components/pages/Home";
-import RootLayout from "./layout";
 
 export default function Page() {
   return (
