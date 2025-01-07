d425aa08d11449a04d3907f0eec2aaba7605f723
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-01-07 20:55:46 +0000

    update db

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,20 +1,22 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-let mongo = (global as any).mongoose || { connection : null , promise : null}
+let mongo = (global as any).mongoose || { connection: null, promise: null };
 
-const mongo_URL = process.env.MONGODB_URI
+const mongo_URL = process.env.MONGODB_URI;
 
-export const connectionDB = async () =>{
-    if(mongo.connection){
-        return mongo.connection
-    }
-    if(!mongo_URL){
-        throw new Error('error connection database')
-    } else {
-        mongo.promise = mongoose.connect( mongo_URL, {
-            dbName : 'Events',
-        })
-        mongo.connection = await mongo.promise
-    }
-    return mongo.connection
-}
+export const connectionDB = async () => {
+  if (mongo.connection) {
+    return mongo.connection;
+  }
+  
+  if (!mongo_URL) {
+    throw new Error("Error: MongoDB URI is missing in environment variables");
+  }
+
+  mongo.promise = mongoose.connect(mongo_URL, {
+    dbName: "Events",
+  });
+  mongo.connection = await mongo.promise;
+
+  return mongo.connection;
+};
