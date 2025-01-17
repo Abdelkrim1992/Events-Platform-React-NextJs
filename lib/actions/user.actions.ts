7956724b1c3a79651dfeeb04eff24e131b572d1a
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-01-17 11:06:12 +0000

    update action in the server

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,47 +1,42 @@
 'user server'
 
 import { CreateUserParams, UpdateUserParams } from '@/types';
-import { connectionDB } from '../database/index';
-import User  from '../models/user.model';
-import Event from '../models/event.model';
-import Category from '../models/category.model';
-import Order from '../models/order.model';
 
-export const CreateUser : any = async(user : CreateUserParams) =>{
+export const CreateUser = async(user : CreateUserParams) =>{
     try {
-        await connectionDB();
-
-        const newUser = await User.create(user);
-
-        return JSON.parse(JSON.stringify(newUser));
+        const newUser = await prisma.user.create({ 
+            data : user,
+         })
+        return newUser;
 
     } catch (error) {
         throw new Error('error in createUser')
     }
 }
 
-export const UpdateUser = async (clerkId : String, user: UpdateUserParams) =>{
+export const UpdateUser = async (Id : string, user: UpdateUserParams) =>{
     try {
 
-        await connectionDB();
+        const updateUser = await prisma.user.update({ 
+            where : { Id },
+            data : user
+        });
 
-        const updateUser = await User.findOneAndUpdate(clerkId, user);
-
-        return JSON.parse(JSON.stringify(updateUser));
+        return updatUser;
 
     } catch (error) {
         throw new Error('error updatedUser')
     }
 }
 
-export const DeleteUser = async(clerkId : String) =>{
+export const DeleteUser = async (Id : string) =>{
     try {
-        
-        await connectionDB();
 
-        const deleteUser = await User.findOne(clerkId);
+        const deleteUser = await prisma.user.delete({ 
+            where : { Id }
+         })
         
-        return JSON.parse(JSON.stringify(deleteUser));
+        return deleteUser;
 
     } catch (error) {
         console.log('error in delete user action')
